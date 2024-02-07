d1196ab2dc1eaceed1c408ad7f63be191d5502dc
ArcadeFortune <alessionights@gmail.com>
2024-02-07 10:33:59 +0100

    try with ws

--- api/index.js
+++ api/index.js
@@ -24,14 +24,28 @@
 // webserver.listen(3000, () => console.log(`Listening on ${3000}`));
 
 
-const app = require('express')();
-const { v4 } = require('uuid');
+const app = require('express')().use((req, res) => res.sendFile("../a.html", { root: __dirname }));
 
+
+const { WebSocketServer } = require("ws");
+const sockserver = new WebSocketServer({ port: 443 });
+
+sockserver.on("connection", (ws) => {
+  console.log("New client connected!");
+  ws.send("connection established");
+  ws.on("close", () => console.log("Client has disconnected!"));
+  ws.on("message", (data) => {
+    sockserver.clients.forEach((client) => {
+      console.log(`distributing message: ${data}`);
+      client.send(`${data}`);
+    });
+  });
+  ws.onerror = function () {
+    console.log("websocket error");
+  };
+});
 app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+  res.end(`Hello! Go to item: <a hrea>`);
 });
 
 app.get('/api/item/:slug', (req, res) => {
